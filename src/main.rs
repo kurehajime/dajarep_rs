1eb9e70179a1ed577d1d58ab9065489cf15195f5
kurehajime <xiidec@gmail.com>
2022-10-25 20:32:53 +0900

    詳細取得

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-use lindera::tokenizer::Tokenizer;
+use lindera::tokenizer::{DictionaryConfig, DictionaryKind, Tokenizer, TokenizerConfig};
 use lindera::LinderaResult;
 
 fn main() -> LinderaResult<()> {
     // create tokenizer
-    let tokenizer = Tokenizer::new()?;
+    let dictionary = DictionaryConfig {
+        kind: DictionaryKind::IPADIC,
+        path: None,
+    };
+    let config = TokenizerConfig {
+        mode: lindera::mode::Mode::Normal,
+        dictionary: dictionary,
+        user_dictionary: None,
+    };
+    let tokenizer = Tokenizer::with_config(config)?;
 
     // tokenize the text
     let tokens = tokenizer.tokenize("東京スカイツリーの最寄り駅はとうきょうスカイツリー駅です")?;
 
     // output the tokens
     for token in tokens {
-        println!("{}", token.text);
+        println!("{},{:?}", token.text, tokenizer.word_detail(token.word_id));
     }
 
     Ok(())
