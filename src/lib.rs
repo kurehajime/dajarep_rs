de2cf29f302258fdc17dc8229427dc65c7c577a9
kurehajime <xiidec@gmail.com>
2022-10-25 22:57:26 +0900

    ちょっとだけリファクタリング

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,22 @@
-use regex::Regex;
-use std::fmt::Error;
-
-use lindera::tokenizer::Tokenizer;
-use sentence::Sentence;
-use word::Word;
-
 mod sentence;
 mod word;
+
+use lindera::tokenizer::Tokenizer;
+use regex::Regex;
+use sentence::Sentence;
+use std::fmt::Error;
+use word::Word;
 
 pub fn is_dajare(sentence: &Sentence) -> Option<String> {
     for word in &sentence.words {
         if word.wtype == "名詞" && word.kana.len() > 1 {
             let w_str = &word.str;
             let w_kana = &word.kana;
-            let str_count = countx(&sentence.str, w_str);
-            let kana_count_1 = countx(&sentence.kana, &fix_word(w_kana));
-            let kana_count_2 = countx(&fix_sentence(&sentence.kana), w_kana);
-            let kana_count_3 = countx(&sentence.yomi, w_kana);
-            let kana_count_4 = countx(&fix_sentence(&sentence.yomi), w_kana);
+            let str_count = count_regex(&sentence.str, w_str);
+            let kana_count_1 = count_regex(&sentence.kana, &fix_word(w_kana));
+            let kana_count_2 = count_regex(&fix_sentence(&sentence.kana), w_kana);
+            let kana_count_3 = count_regex(&sentence.yomi, w_kana);
+            let kana_count_4 = count_regex(&fix_sentence(&sentence.yomi), w_kana);
             let kana_array = vec![kana_count_1, kana_count_2, kana_count_3, kana_count_4];
             let max_kana_count = kana_array.iter().max().unwrap();
 
@@ -30,7 +29,7 @@ pub fn is_dajare(sentence: &Sentence) -> Option<String> {
     None
 }
 
-fn countx(text: &str, target: &str) -> i32 {
+fn count_regex(text: &str, target: &str) -> i32 {
     let re = Regex::new(target).unwrap();
     let caps = re.find_iter(text).count();
     caps.try_into().unwrap()
@@ -48,18 +47,18 @@ fn fix_word(text: &str) -> String {
     let text = text.replace("ヅ", "[ズヅ]");
     let text = text.replace("ヂ", "[ジヂ]");
     let text = text.replace("ジ", "[ジヂ]");
-    let text = replacex(text, "([アカサタナハマヤラワャ])ー", "$1[アァ]?");
-    let text = replacex(text, "([イキシチニヒミリ])ー", "$1[イィ]?");
-    let text = replacex(text, "([ウクスツヌフムユルュ])ー", "$1[ウゥ]?");
-    let text = replacex(text, "([エケセテネへメレ])ー", "$1[エェ]?");
-    let text = replacex(text, "([オコソトノホモヨロヲョ])ー", "$1[ウゥオォ]?");
+    let text = replace_regex(&text, "([アカサタナハマヤラワャ])ー", "$1[アァ]?");
+    let text = replace_regex(&text, "([イキシチニヒミリ])ー", "$1[イィ]?");
+    let text = replace_regex(&text, "([ウクスツヌフムユルュ])ー", "$1[ウゥ]?");
+    let text = replace_regex(&text, "([エケセテネへメレ])ー", "$1[エェ]?");
+    let text = replace_regex(&text, "([オコソトノホモヨロヲョ])ー", "$1[ウゥオォ]?");
     let text = text.replace("ャ", "[ヤャ]");
     let text = text.replace("ュ", "[ユュ]");
     let text = text.replace("ョ", "[ヨョ]");
     let text = text.replace("ー", "[ー]?");
     text
 }
-fn replacex(text: String, from: &str, to: &str) -> String {
+fn replace_regex(text: &str, from: &str, to: &str) -> String {
     Regex::new(from).unwrap().replace_all(&text, to).to_string()
 }
 //本文から省略可能文字を消したパターンを返す。
