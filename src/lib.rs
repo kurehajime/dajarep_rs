3affec6790db1c1838cbb798c66345aed4f1ce30
kurehajime <xiidec@gmail.com>
2022-11-01 22:54:39 +0900

    テスト追加

--- src/lib.rs
+++ src/lib.rs
@@ -141,3 +141,44 @@ fn count_regex(text: &str, target: &str) -> i32 {
     let caps = re.find_iter(text).count();
     caps.try_into().unwrap()
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_dajarep() {
+        let input = r#"人民の人民による人民のための政治
+アルミ缶の上にあるミカン
+トンネルを抜けるとそこは雪国であった
+智代子のチョコ
+布団が吹っ飛んだ
+我輩は猫である
+猫が寝転んだ
+その意見にはついていけん
+靴を靴箱に入れる
+傘を貸さない
+イカは如何なものか
+親譲りの無鉄砲で子供の時から損ばかりしている
+マイケル・ジョーダンが冗談を言った
+知事が縮む
+鶏には取り憑かない
+破壊についての和解"#;
+        let answer = r#"アルミ缶の上にあるミカン
+智代子のチョコ
+布団が吹っ飛んだ
+猫が寝転んだ
+その意見にはついていけん
+傘を貸さない
+イカは如何なものか
+マイケル・ジョーダンが冗談を言った
+知事が縮む
+鶏には取り憑かない"#
+            .split("\n");
+
+        let result = dajarep(input).unwrap();
+        answer.zip(result.iter()).for_each(|(a, r)| {
+            assert_eq!(a, r);
+        });
+    }
+}
